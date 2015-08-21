cc111efb23bd965340c04790e8da12322f496e94
Florian <flofank@gmx.ch>
2015-08-21 17:30:14 +0200

    smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 /**
  * Created by flofank on 18.08.2015.
  */
+$('a[href*=#]').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+    return false;
+});
